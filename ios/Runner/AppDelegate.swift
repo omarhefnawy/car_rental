5bb02a5ae2ad8af5947a78a44090e31d38b9b123
omarh <omarhefhef90@gmail.com>
2025-07-18 02:20:21 +0300

    safty

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,7 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GMSServices.provideAPIKey("AIzaSyABKSTf-9-q9e4bOiyYHl4m3pEnoet6ofk")
+    GMSServices.provideAPIKey("")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:nested/nested.dart';
+import 'package:device_frame/device_frame.dart'; // هنا الإضافة الجديدة
 import 'core/service_locators/service_locator.dart';
 import 'features/auth/domain/authrepo/auth_repo.dart';
 import 'features/auth/presentation/screens/regester.dart';
@@ -16,17 +17,14 @@ import 'features/cars/presetation/screens/addCar.dart';
 import 'features/home/presentation/screens/home.dart';
 import 'firebase_options.dart';
 
-void main()async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // for firebase
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  //service locator
   initServiceLocator();
-  // instance of Cache helper
   await CacheHelper.init();
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -36,23 +34,32 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiBlocProvider(
       providers: <SingleChildWidget>[
-       BlocProvider(create: (context) => AuthCubit(sl<AuthRepo>()),),
-        BlocProvider(create: (context) => CarCubit(sl<CarRepo>()),),
+        BlocProvider(create: (context) => AuthCubit(sl<AuthRepo>())),
+        BlocProvider(create: (context) => CarCubit(sl<CarRepo>())),
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        initialRoute: ("/"),
+        initialRoute: "/",
         routes: {
-          "/": (context) => SplashScreen(),
-          "login": (context) => SignInPage(),
-          "signUp": (context) => RegisterPage(),
-          "onboard": (context) => OnboardingScreen(),
-          "home": (context) => HomeScreen(),
-          "addCar": (context) => AddCar(),
+          "/": (context) => wrapInDevice(const SplashScreen()),
+          "login": (context) => wrapInDevice(SignInPage()),
+          "signUp": (context) => wrapInDevice(RegisterPage()),
+          "onboard": (context) => wrapInDevice(const OnboardingScreen()),
+          "home": (context) => wrapInDevice(const HomeScreen()),
+          "addCar": (context) => wrapInDevice(AddCar()),
         },
       ),
     );
   }
+
+  /// دالة بتغلف أي شاشة داخل إطار موبايل
+  Widget wrapInDevice(Widget screen) {
+    return Center(
+      child: DeviceFrame(
+        device: Devices.android.bigPhone, // تقدر تغيّر نوع الجهاز هنا
+        isFrameVisible: true,
+        screen: screen,
+      ),
+    );
+  }
 }
-
-
